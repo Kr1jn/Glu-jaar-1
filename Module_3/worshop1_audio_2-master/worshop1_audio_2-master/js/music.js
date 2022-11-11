2a26fee1425bb6f5a8ebd5476aac6fed0b433677
Kr1jn <113419502+Kr1jn@users.noreply.github.com>
2022-11-11 10:31:06 +0100

    yes

---
+++ Module_3/worshop1_audio_2-master/worshop1_audio_2-master/js/music.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+  {
+    name: "I Think i'm Normal",
+    artist: "Carter Ace",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Come and get your Love",
+    artist: "Redbone",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "No woman, No cry",
+    artist: "Tems",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Enemy",
+    artist: "Imagine Dragons",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Happy",
+    artist: "Pharrell Williams",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Something just like this",
+    artist: "The Chainsmokers & Coldplay",
+    img: "music-6",
+    src: "music-6"
+  },
+];
